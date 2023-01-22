4052e9b9064bf06a22f8d23addba028de0f5bf53
TheTopCat84 <119445189+TheTopCat84@users.noreply.github.com>
2023-01-22 00:10:37 +0000

    Update module.chartCPU.js

--- module.chartCPU.js
+++ module.chartCPU.js
@@ -1,37 +1,78 @@
 global.chartCPU = function() {
+    // collect data
     let x = 0,
     xpoints = [],
-    maxXValue = CPUManager.length,
-    maxYValue = Math.max(...CPUManager);
+    maxXValue = CPU.length,
+    maxYValue = Math.max(...CPU);
     
-    for (let value of CPUManager) {
+    for (let value of CPU) {
     	let y = value / maxYValue,
-    	xpoint = 46 * x / maxXValue + 2;
-    	xpoints.push([xpoint, 10 - 10*y]);
+    	xpoint = 44 * x / maxXValue + 4;
+    	xpoints.push([xpoint, 12 - 10*y]);
     	x++;
     }
     
-    new RoomVisual().rect(0, 0, 49 * x / maxXValue, 13, {fill: 'black'});
+    // Background
+    new RoomVisual().rect(0, 0, 49, 29, {fill: 'black'});
 
-    new RoomVisual().line(2, 10, 45 * x / maxXValue + 5, 10, {color: 'white', lineStyle: 'solid'});
-    new RoomVisual().text("0",1, 10);
+    // CPU line chart
+    new RoomVisual().text("CPU/tick",24.5, 1);
+    
+    new RoomVisual().line(4, 12, 44 * x / maxXValue + 5, 12, {color: 'white', lineStyle: 'solid'});
+    new RoomVisual().text("0",2, 12);
 
     new RoomVisual().poly(xpoints, {color: 'white', lineStyle: 'solid'});
     
-    new RoomVisual().line(2, 10 - 10*(20/maxYValue), 44 * x / maxXValue + 5, 10 - 10*(20/maxYValue), {color: 'red', lineStyle: 'dashed'});
-    new RoomVisual().text("20",1, 10 - 10*(20/maxYValue));
-
-    new RoomVisual().line(2, 10 - 10*(10/maxYValue), 44 * x / maxXValue + 5, 10 - 10*(10/maxYValue), {color: 'red', lineStyle: 'dashed'});
-    new RoomVisual().text("10",1, 10 - 10*(10/maxYValue));
+    new RoomVisual().line(4, 12 - 10*(20/maxYValue), 44 * x / maxXValue + 5, 12 - 10*(20/maxYValue), {color: 'red', lineStyle: 'dashed'});
+    new RoomVisual().text("20",2, 12 - 10*(20/maxYValue));
     
-    new RoomVisual().line(2, 10 - 10*(30/maxYValue), 44 * x / maxXValue + 5, 10 - 10*(30/maxYValue), {color: 'red', lineStyle: 'dashed'});
-    new RoomVisual().text("30",1, 10 - 10*(30/maxYValue));
+    new RoomVisual().line(4, 12 - 10*(10/maxYValue), 44 * x / maxXValue + 5, 12 - 10*(10/maxYValue), {color: 'red', lineStyle: 'dashed'});
+    new RoomVisual().text("10",2, 12 - 10*(10/maxYValue));
     
-    new RoomVisual().line(2, 10 - 10*((_.sum(CPUManager)/CPUManager.length)/maxYValue), 44 * x / maxXValue + 5, 10 - 10*((_.sum(CPUManager)/CPUManager.length)/maxYValue), {color: 'green', lineStyle: 'dashed'});
-    new RoomVisual().text("Avg",1, 10 - 10*(_.sum(CPUManager)/CPUManager.length)/maxYValue,{color: 'green'});
+    new RoomVisual().line(4, 12 - 10*(30/maxYValue), 44 * x / maxXValue + 5, 12 - 10*(30/maxYValue), {color: 'red', lineStyle: 'dashed'});
+    new RoomVisual().text("30",2, 12 - 10*(30/maxYValue));
+    
+    new RoomVisual().line(4, 12 - 10*((_.sum(CPU)/CPU.length)/maxYValue), 44 * x / maxXValue + 5, 12 - 10*((_.sum(CPU)/CPU.length)/maxYValue), {color: 'green', lineStyle: 'dashed'});
+    new RoomVisual().text("Avg",2, 12 - 10*(_.sum(CPU)/CPU.length)/maxYValue,{color: 'green'});
 
-    new RoomVisual().text("Avg: " + Math.round(100*(_.sum(CPUManager)/CPUManager.length))/100,20, 12,{color: 'white', strokeWidth: 0.2});
-    new RoomVisual().text("Min: " + Math.round(100*_.min(CPUManager))/100,30, 12,{color: 'white', strokeWidth: 0.2});
-    new RoomVisual().text("Bucket: " + Game.cpu.bucket, 10, 12,{color: 'white', strokeWidth: 0.2});
-    new RoomVisual().text("# ticks: " + CPUManager.length, 40, 12,{color: 'white', strokeWidth: 0.2});
+    let i = 0,
+    ipoints = [],
+    maxIValue = bucket.length,
+    minJValue = Math.floor((Math.min(...bucket) - 50)/100)*100;
+    
+    for (let value of bucket) {
+    	let v = (value-minJValue) / (10000-minJValue),
+    	ipoint = 44 * i / maxIValue + 4;
+    	ipoints.push([ipoint, 25 - 10*v]);
+    	i++;
+    }
+    
+    // Bucket line chart
+    new RoomVisual().text("CPU Bucket",24.5, 14);
+    
+    new RoomVisual().line(4, 25, 44 * i / maxIValue + 5, 25, {color: 'white', lineStyle: 'solid'});
+    new RoomVisual().text(minJValue,2, 25);
+
+    new RoomVisual().poly(ipoints, {color: 'white', lineStyle: 'solid'});
+    
+    new RoomVisual().line(4, 20, 44 * i / maxIValue + 5, 20, {color: 'red', lineStyle: 'dashed'});
+    new RoomVisual().text((10000+minJValue)/2,2, 20);
+    
+    new RoomVisual().line(4, 15, 44 * i / maxIValue + 5, 15, {color: 'red', lineStyle: 'dashed'});
+    new RoomVisual().text("10000",2, 15);
+    
+    // Verticals
+    let numberOfVerticals = Math.floor(maxIValue/50);
+    for(let i = 1; i <= numberOfVerticals; i++) {
+        new RoomVisual().line(4+i*44*50/maxIValue, 2, 4+i*44*50/maxIValue, 25, {color: 'white', lineStyle: 'dashed'});
+        new RoomVisual().text(50*i,4+44*i*50/maxIValue, 13);
+        new RoomVisual().text(50*i,4+44*i*50/maxIValue, 26);
+    }
+
+    // Stats
+    new RoomVisual().text("Avg: " + Math.round(100*(_.sum(CPU)/CPU.length))/100,20, 28,{color: 'white', strokeWidth: 0.2});
+    new RoomVisual().text("Min: " + Math.round(100*_.min(CPU))/100,30, 28,{color: 'white', strokeWidth: 0.2});
+    new RoomVisual().text("Bucket: " + Game.cpu.bucket, 10, 28,{color: 'white', strokeWidth: 0.2});
+    new RoomVisual().text("# ticks: " + CPU.length, 40, 28,{color: 'white', strokeWidth: 0.2});
+    
 }
